be59cd563b59aefd946761052513c923471443cb
theninexl <thenine@thenine.es>
2024-04-10 11:25:14 +0200

    adding any google font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,7 @@
-import { Montserrat } from 'next/font/google';
+import { Lusitana, Montserrat } from 'next/font/google';
 
 export const montserrat = Montserrat({subsets: ['latin'], weight: '400'});
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+})
